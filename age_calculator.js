f62ca6c43fb834c1af0793cff32163ab90e81fe5
Erdogan <kopuskopecik@gmail.com>
2023-04-18 09:24:26 +0200

    add age calculator

---
+++ age_calculator.js
@@ -0,0 +1,15 @@
+const myBirthYear = 1987;
+const futureYear1 = 2056;
+const futureYear2 = 2034;
+let message1;
+let message2;
+
+const myAge1 = futureYear1 - myBirthYear;
+const myAge2 = futureYear2 - myBirthYear;
+
+message1 = `I will be ${myAge1} in ${futureYear1}`;
+message2 = `I will be ${myAge2} in ${futureYear2}`;
+
+console.log(message1);
+console.log(message2);
+
