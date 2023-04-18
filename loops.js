a1b9d900f047fc462b085e3e0350c53d383182ab
Erdogan <kopuskopecik@gmail.com>
2023-04-18 10:12:04 +0200

    add all lopps examples

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 const myList = [1, "apple", 2, 3, 4];
-
+let i;
 while (i < 10) {
   text += "The number is " + i;
   i++;
@@ -12,3 +12,24 @@ for (let x = 0; x < 5; x++) {
 myList.forEach(function (value, index) {
   console.log(index, value);
 });
+
+for (key in "appleeeee") {
+  console.log(key);
+}
+
+const person = { name: "John", surname: "Doe", age: 25 };
+
+for (let x in person) {
+  console.log(x);
+}
+
+let language = "JavaScript";
+
+for (let x of language) {
+  console.log(x);
+}
+
+do {
+  // The loop will always be executed at least once, even if the condition is false
+console.log('at least one time will run')
+} while (false);
