ffdcd7e95da2bfa0a2b373a37760fb7442f659fc
Erdogan <kopuskopecik@gmail.com>
2023-04-18 09:41:02 +0200

    add else if structure

--- tutorial.js
+++ tutorial.js
@@ -1,25 +1,32 @@
 const logic = true;
+const myNumber = 10;
 const sutuation = 1;
 
 if (logic) {
-    console.log('I am true')
+  console.log("I am true");
+} else {
+  console.log("I am false. sorry");
 }
 
-else {
-    console.log('I am false. sorry')
+if (myNumber >= 12) {
+  console.log("I am bigger than 12.");
+} else if (myNumber >= 10) {
+  console.log("I am bigger than 10.");
+} else {
+  console.log("I dont know what i am.");
 }
 
 switch (2) {
-    case 1:
-        console.log('I am one')
-        break;
-    case 2:
-        console.log('I am two')
-        break;
-    case 3:
-        console.log('I am three')
-        break;
+  case 1:
+    console.log("I am one");
+    break;
+  case 2:
+    console.log("I am two");
+    break;
+  case 3:
+    console.log("I am three");
+    break;
 
-    default:
-        break;
+  default:
+    break;
 }
