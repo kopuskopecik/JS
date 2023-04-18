93621ca7b65f5eb6ad5d034185f8b7f3e7f413b0
Erdogan <kopuskopecik@gmail.com>
2023-04-18 09:31:13 +0200

    add if and switch structure

--- tutorial.js
+++ tutorial.js
@@ -1,4 +1,5 @@
 const logic = true;
+const sutuation = 1;
 
 if (logic) {
     console.log('I am true')
@@ -6,4 +7,19 @@ if (logic) {
 
 else {
     console.log('I am false. sorry')
+}
+
+switch (2) {
+    case 1:
+        console.log('I am one')
+        break;
+    case 2:
+        console.log('I am two')
+        break;
+    case 3:
+        console.log('I am three')
+        break;
+
+    default:
+        break;
 }
