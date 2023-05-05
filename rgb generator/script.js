1c41e09903327158f77fbcbf3952398751761e08
Erdogan <kopuskopecik@gmail.com>
2023-05-05 11:01:25 +0200

    add rgb generator

---
+++ rgb generator/script.js
@@ -0,0 +1,22 @@
+const red = document.querySelector('#red');
+const green = document.querySelector('#green');
+const blue = document.querySelector('#blue');
+const main = document.querySelector('main');
+const paragraph = document.querySelector('p');
+
+const callEvents = ()=>{
+    main.addEventListener('input', changeBackgroundColor)
+}
+
+const changeBackgroundColor = (event)=>{
+    console.log(event.target)
+    const redValue = Number(red.value);
+    const greenValue = Number(green.value);
+    const blueValue = Number(blue.value);
+    const hexValue = "#"+(redValue).toString(16)+(greenValue).toString(16)+(blueValue).toString(16);
+    main.style.backgroundColor = hexValue
+    paragraph.textContent = `Red: ${redValue}, Green:${greenValue} and Blue: ${blueValue}`
+}
+
+callEvents()
+
